3a3d25e3041823ce03d5b69afee7bab54ecd43d8
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-26 18:42:12 +0530

    actions

---
+++ mysql.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell_script_roboshop" # This variable is used to store the logs of the script in a specific folder, which is useful for troubleshooting and debugging purposes. The user can check the logs to see if there were any errors or issues during the execution of the script, and it can also be used to track the progress of the script. The user should ensure that they have the necessary permissions to create and write to this folder, and it is recommended to use a folder that is not easily accessible to unauthorized users for security reasons.
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R='\e[0;31m'
+G='\e[0;32m'
+Y='\e[0;33m'
+N='\e[0m'
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>> $LOGS_FILE
+systemctl start mysqld
+VALIDATE $? "Enabling and Starting MySQL service"
+
+# Get the password from the User
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up MySQL root password"

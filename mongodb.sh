be50814e818a008a450b7d97e965b85a13c20c73
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-26 11:58:29 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell_script_roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R='\e[0;31m'
+G='\e[0;32m'
+Y='\e[0;33m'
+N='\e[0m'
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "Copying MongoDB repo file"
+
+dnf install mongodb-org -y
+VALIDATE $? "Installing MongoDB Server"
+
+systemctl enable mongod
+VALIDATE $? "Enabling MongoDB Service"
+
+systemctl start mongod
+VALIDATE $? "Starting MongoDB Service"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB Service"

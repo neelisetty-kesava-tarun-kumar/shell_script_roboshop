442e6497fa6251770df9409409dd3c8c64e59c2f
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-04-26 11:18:13 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 SG_ID="sg-0075bb69cff1075c4" #Should replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5" #Should replace with your AMI ID
+ZONE_ID="Z00664892VVKTE5BFORKG" #Should replace with your Hosted Zone ID
+DOMAIN_NAME="kesavatarun.in" #Should replace with your domain name
 
 for instance in $@
 do
@@ -20,6 +22,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
+        RECORD_NAME="$DOMAIN_NAME" # This will create a record name with the domain name of the frontend domain only like "domain_name.com" for the frontend instance.
     else
         IP=$(
             aws ec2 describe-instances \
@@ -27,7 +30,32 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # This will create a record name like "catalog.kesavatarun.in" for the catalog instance, and "user.kesavatarun.in" for the user instance, and so on. This way, we can easily access the instances using their respective domain names.
     fi
 
-    echo "IP Address of $instance = $IP"
+    echo "IP Address of $instance = $IP" # This will print the IP address of the instance in the terminal, which is useful for the user to know the IP address of the instance that has been created. This can be used for further configuration or troubleshooting if needed.
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating A record for '$instance'",
+        "Changes": [
+            {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 300,
+                    "ResourceRecords": [
+                        {
+                            "Value": "'$IP'"
+                        }
+                    ]
+                }
+            }
+        ]
+    }'
+    
+    echo "A record is updated for $instance with IP $IP" # This will print a message in the terminal indicating that the A record has been updated for the instance with the corresponding IP address. This is useful for the user to know that the script has successfully updated the DNS records for the instances.
 done
